a99f62b3fb4b1f07a786c9fd932c3a23b0403441
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-04 09:54:05 -0300

    Initial commit

---
+++ lab_dgs/src/main/java/com/gabrielbazante/lab_dgs/LabDgsApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbazante.lab_dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDgsApplication.class, args);
+	}
+
+}
---
+++ lab_dgs/src/test/java/com/gabrielbazante/lab_dgs/LabDgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbazante.lab_dgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

dc780b8a9bb1a9865948e95dae377ca3f8d639ee
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-04 13:25:43 -0300

    6 - Criada abstração de cache do spring para adicionar cache ao app

--- lab_dgs/src/main/java/com/gabrielbazante/lab_dgs/LabDgsApplication.java
+++ lab_dgs/src/main/java/com/gabrielbazante/lab_dgs/LabDgsApplication.java
@@ -2,8 +2,10 @@ package com.gabrielbazante.lab_dgs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LabDgsApplication {
 
 	public static void main(String[] args) {
---
+++ lab_dgs/src/main/java/com/gabrielbazante/lab_dgs/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.gabrielbazante.lab_dgs.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.gabrielbazante.lab_dgs.exceptions.ProductNotFoundException;
+import com.gabrielbazante.lab_dgs.model.Product;
+import com.gabrielbazante.lab_dgs.repository.ProductRepository;
+
+
+
+@Service
+@CacheConfig(cacheNames = "products")
+public class ProductService {
+ 
+    @Autowired
+    private ProductRepository productRepository;
+ 
+    @Cacheable
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+ 
+    @Cacheable(key = "#id", unless = "#result == null")
+    public Product getProductById(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            return optionalProduct.get();
+        }
+        throw new ProductNotFoundException("Product not found with id " + id);
+    }
+ 
+    @CachePut(key = "#product.id")
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+ 
+    @CacheEvict(key = "#id")
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+ 
+}
+
